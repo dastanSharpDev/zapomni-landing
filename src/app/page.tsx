47dfbc64f58e3186060fe9abe62017a9a1add179
dastanSharpDev <your-email@example.com>
2025-09-20 23:50:58 +0500

    changed images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import Partners from "@/components/Partners";
 import Footer from "@/components/Footer";
-import CtaAsk from "@/components/CtaAsk";
+// import CtaAsk from "@/components/CtaAsk";
 import CashierAutomation from "@/components/CashierAutomation";
 
 export default function Home() {
